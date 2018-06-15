5d77d0687f32323a014ece073d9e83fa0f0ac50f
stanleysq <10691963+stanleysq@users.noreply.github.com>
2018-06-15 23:03:54 +0800

    Support getting movie artworks from iTunes search API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react"
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider"
+import getMuiTheme from "material-ui/styles/getMuiTheme"
+import { amber500 } from "material-ui/styles/colors"
+import RaisedButton from "material-ui/RaisedButton"
+import TextField from "material-ui/TextField"
+import LazyLoad from "react-lazyload"
+import "./App.css"
 
 const HULU_ASSET_HOST = "http://assets.huluim.com"
 const huluMovieArtworkUrl = title => {
@@ -9,66 +15,115 @@ const huluMovieArtworkUrl = title => {
   return `${HULU_ASSET_HOST}/movies/movie${part}.jpg`
 }
 
+const ITUNES_SEARCH_API_BASE = "https://itunes.apple.com/search"
+
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = { query: "" }
+    this.state = { entries: [] }
   }
 
   handleSubmit = e => {
     e.preventDefault()
-    const query = document.getElementById("query").value
-    this.setState({ query: query })
+    this.setState({ entries: [] })
+    const input = document.getElementById("input").value
+
+    if (input.startsWith("http")) {
+      const matchResults = input.match(/(^https?:\/\/.+)?(https?:\/\/.+.(jpg|png))/)
+      if (matchResults) {
+        let artworkUrl = matchResults[2]
+        const extentions = ["jpg", "png"]
+        extentions.forEach((e) => {
+          let regExp = new RegExp(`(_w\\d{3}|_SL1500_).${e}$`)
+          artworkUrl = artworkUrl.replace(regExp, `_original.${e}`)
+          regExp = new RegExp(`_small.${e}$`)
+          artworkUrl = artworkUrl.replace(regExp, `_large.${e}`)
+        })
+        this.setState({ entries: [{ artworkUrl }] })
+      } else { alert("Invalid URL") }
+    } else {
+      const capitalize = str => (str.charAt(0).toUpperCase() + str.slice(1))
+      const titleCase = str => {
+        let words = str.split(" ")
+        const nonPrincipalWords = [
+          "a", "an", "the",
+          "and", "or", "but", "nor", "yet", "so",
+          "as", "at", "by", "for", "in", "mid", "of", "off", "on", "per", "pro", "qua", "to", "via", "vs."
+        ]
+        words = words.map((word, i) => (
+          i === 0 || nonPrincipalWords.indexOf(word) > 0 ? word :capitalize(word)
+        ))
+        return capitalize(words.join(" "))
+      }
+      const huluEntry = {
+        title: titleCase(input), artworkUrl: huluMovieArtworkUrl(input)
+      }
+      let iTunesURL = new URL(ITUNES_SEARCH_API_BASE)
+      const searchParams = { "term": input, media: "movie" }
+      Object.keys(searchParams).forEach(key => {
+        iTunesURL.searchParams.append(key, searchParams[key])
+      })
+      fetch(iTunesURL).then((res) => {
+        console.log(res)
+        res.json().then((json) => {
+          console.log(json)
+          const entries = json.results.map((result => {
+            const title = result.trackName
+            const artist = result.artistName
+            const year = result.releaseDate.slice(0, 4)
+            const artworkUrl = result.artworkUrl100.replace(
+              /100x100bb\.jpg$/, "100000x100000-999.jpg"
+            )
+            return { title, artist, year, artworkUrl }
+          }))
+          entries.unshift(huluEntry)
+          this.setState({ entries })
+        })
+      })
+    }
   }
 
   render() {
+    const muiTheme = getMuiTheme({
+      fontFamily: "inherit",
+      palette: {
+        primary1Color: amber500
+      },
+      raisedButton: {
+        color: "rgba(158, 158, 158, 0.2)",
+        fontSize: "1.3rem",
+        fontWeight: "600"
+      }
+    })
+
     return (
-      <div className="App">
-        <article>
-      		<h3>Get Artwork</h3>
-      		<form onSubmit={this.handleSubmit}>
-            <div>
-        			<select name="entity" id="entity">
-                <option value="movie">Movie</option>
-                {/*
-        				  <option value="tvSeason">TV Show</option>
-                  <option value="shortFilm">Short Film</option>
-                  <option value="album">Album</option>
-                  <option value="id">Apple ID (Movie)</option>
-                  <option value="idAlbum">Apple ID (Album)</option>
-                */}
-                {/*
-                  <option value="musicVideo">Music Video (may not work)</option>
-                  <option value="ebook">iBook</option>
-                  <option value="audiobook">Audiobook</option>
-        				  <option value="software">App</option>
-                  <option value="podcast">Podcast</option>
-                */}
-        			</select>
-        			<select name="country" id="country">
-        				<option value="us">USA</option>
-        				{/* <option value='gb'>UK</option> */}
-        			</select>
-            </div>
-            <div>
-              <input type="text" id="query" />
-              <input type="submit" value="Find" />
-            </div>
-      		</form>
-
-
-          {
-            this.state.query &&
-              <div>
-                <img
-                  src={huluMovieArtworkUrl(this.state.query)}
-                  alt={this.state.query} height="600"
-                />
-              </div>
-          }
-
-        </article>
-      </div>
+      <MuiThemeProvider {...{ muiTheme }}>
+        <div className="app">
+          <nav>
+        		<form onSubmit={this.handleSubmit}>
+              <TextField id="input" style={{ width: 800 }} />
+              <RaisedButton label="Get" type="submit" />
+        		</form>
+          </nav>
+          <div className="results">
+            {
+              this.state.entries.map((entry, i) => (
+                <div style={{ display: "inline-block", width: "30%", overflow: "hidden", verticalAlign: "top" }} key={i}>
+                  <div className="line-1">
+                    <a href={entry.artworkUrl}>{entry.title}</a>
+                  </div>
+                  <div className="line-2">
+                    {entry.artist} {entry.year && `(${entry.year})`}
+                  </div>
+                  <LazyLoad height={600} >
+                    <img src={entry.artworkUrl} alt={entry.title} height="600" />
+                  </LazyLoad>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
